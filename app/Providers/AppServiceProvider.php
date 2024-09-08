e195f61efd134d2a11695c619de7e4736e15f7b3
Ndubuisi O. <ndollawa@yahoo.com>
2024-09-09 00:21:52 +0100

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class AppServiceProvider extends AppServiceProvider
 {
     /**
      * Register any application services.
