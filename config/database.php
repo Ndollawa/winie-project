b98b9e8cb2c177a5960e5f3b9d8279e9bfdc6a96
Ndubuisi O. <ndollawa@yahoo.com>
2024-09-09 00:30:47 +0100

    first commit

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'dpg-crf2pojgbbvc73bv5810-a'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'winie_project'),
+            'username' => env('DB_USERNAME', 'winie_project_user'),
+            'password' => env('DB_PASSWORD', 'VzEgPQgkJfpeXQMPyQU6NljbTv4WmDzR'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
